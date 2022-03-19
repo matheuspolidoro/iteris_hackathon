e8ff7f1c31922ffff0d02de01055b9619dd9249f
Carlos Olympio Santos De Melo <carlosolympio0420@gmail.com>
2022-03-19 11:42:30 -0300

    Co-authored-by: matheuspolidoro <matheuspolidoro@outlook.com>

---
+++ src/views/DetalheView.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    <h1>This is a dynamic route</h1>
+    <h2>The route param is: {{ this.$route.params.id }}</h2>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "DetalheView",
+};
+</script>
