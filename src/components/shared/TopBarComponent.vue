5d702c6b86dc31a26eee0d62a0253dc93add578b
Iasmin Santos <iasminwinx@gmail.com>
2022-03-19 12:01:12 -0300

    primeira versao do login

--- src/components/shared/TopBarComponent.vue
+++ src/components/shared/TopBarComponent.vue
@@ -22,12 +22,12 @@
             </v-list-item>
           </router-link>
 
-          <router-link to="/classificacao">
+          <router-link to="/Entrar">
             <v-list-item>
               <v-list-item-icon>
                 <v-icon>mdi-account</v-icon>
               </v-list-item-icon>
-              <v-list-item-title> Classificação </v-list-item-title>
+              <v-list-item-title> Entrar </v-list-item-title>
             </v-list-item>
           </router-link>
         </v-list-item-group>
