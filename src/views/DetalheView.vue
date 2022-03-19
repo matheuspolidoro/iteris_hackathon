4a37fad5ee197af9838d88348a865e728e14ca79
matheuspolidoro <matheuspolidoro@outlook.com>
2022-03-19 13:24:37 -0300

    inclusao google map na detalhe view

--- src/views/DetalheView.vue
+++ src/views/DetalheView.vue
@@ -1,32 +1,107 @@
 <template>
   <v-container>
-    <h1>This is a dynamic route</h1>
-    <h2>The route param is: {{ this.$route.params.id }}</h2>
-    <h3> {{blocoObject.name}} </h3>
+    <v-card :loading="loading" class="mx-auto my-12" max-width="680px">
+      <template slot="progress">
+        <v-progress-linear
+          color="deep-purple"
+          height="10"
+          indeterminate
+        ></v-progress-linear>
+      </template>
+
+      <v-img height="250" v-bind:src="blocoObject.photo"></v-img>
+
+      <v-card-title>{{ blocoObject.name }}</v-card-title>
+
+      <v-card-text>
+        <v-row align="center" class="mx-0">
+          <v-rating
+            :value="4.5"
+            color="amber"
+            dense
+            half-increments
+            readonly
+            size="14"
+          ></v-rating>
+
+          <div class="grey--text ms-4">4.5 (413)</div>
+        </v-row>
+
+        <div class="my-4 text-subtitle-1">$ • Italian, Cafe</div>
+
+        <div>
+          Small plates, salads & sandwiches - an intimate setting with 12 indoor
+          seats plus patio seating.
+          <h2>The route param is: {{ this.$route.params.id }}</h2>
+        </div>
+      </v-card-text>
+
+      <v-divider class="mx-4"></v-divider>
+
+      <v-card-title>Tonight's availability</v-card-title>
+
+      <v-card-text>
+        <v-chip-group
+          v-model="selection"
+          active-class="deep-purple accent-4 white--text"
+          column
+        >
+          <v-chip>5:30PM</v-chip>
+
+          <v-chip>7:30PM</v-chip>
+
+          <v-chip>8:00PM</v-chip>
+
+          <v-chip>9:00PM</v-chip>
+        </v-chip-group>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-btn color="deep-purple lighten-2" text @click="reserve">
+          Reserve
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card :loading="loading" class="mx-auto my-12" max-width="680px">
+      <div class="mapouter">
+        <div class="gmap_canvas">
+          <iframe
+            width="100%"
+            height="400px"
+            id="gmap_canvas"
+            :src="bloco_map_url"
+            frameborder="0"
+            scrolling="no"
+            marginheight="0"
+            marginwidth="0"
+          ></iframe>
+        </div>
+      </div>
+    </v-card>
   </v-container>
 </template>
-
 <script>
- 
 export default {
   name: "DetalheView",
   data: function () {
     return {
-        prop: 'aaaaa',
-        prop1: 'dsdsd',
-        prop4: 'eopdkso',
-        prop5: 'eopdks',
-
-        blocoObject: {
-            address: "Salvador - BA",
-            description: "Considerado um dos mais antigos blocos de Salvador, o bloco Mudança do Garcia existe há 82 anos e sai de um dos circuitos oficiais do carnaval, o Circuito Osmar. A agremiação se destaca pelos protestos realizados pelos foliões durante o cortejo. Com faixas, cartazes e fantasias tratando de assuntos como política, educação e saúde os foliões saem pelas ruas do Campo Grande dançando, fazendo brincadeiras e gritando palavras de ordem.",
-            name: "Mudança do Garcia",
-            photo: "https://www.correio24horas.com.br/fileadmin/_processed_/2/d/csm_faixa_carnaval_de1dfb906e.jpg"
-        }
-    }
+      bloco_map_url: "",
+      blocoObject: {
+        address: "Salvador - BA",
+        description:
+          "Considerado um dos mais antigos blocos de Salvador, o bloco Mudança do Garcia existe há 82 anos e sai de um dos circuitos oficiais do carnaval, o Circuito Osmar. A agremiação se destaca pelos protestos realizados pelos foliões durante o cortejo. Com faixas, cartazes e fantasias tratando de assuntos como política, educação e saúde os foliões saem pelas ruas do Campo Grande dançando, fazendo brincadeiras e gritando palavras de ordem.",
+        name: "Mudança do Garcia",
+        photo:
+          "https://www.correio24horas.com.br/fileadmin/_processed_/2/d/csm_faixa_carnaval_de1dfb906e.jpg",
+      },
+    };
   },
   created: function () {
-    console.log('bloco é: ' + this.blocoObject)
-  }
+    console.log("bloco é: " + this.blocoObject);
+    this.bloco_map_url =
+      "https://maps.google.com/maps?q=" +
+      "Juiz de Fora" +
+      "&t=&z=13&ie=UTF8&iwloc=&output=embed";
+  },
 };
 </script>
